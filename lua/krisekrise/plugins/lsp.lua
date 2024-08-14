fda747d177c4b66216ea902515dec5a34e099d28
stilllovinnero <9414276+stilllovinnero@users.noreply.github.com>
2024-08-14 10:10:27 +0200

    added typescript support

--- lua/krisekrise/plugins/lsp.lua
+++ lua/krisekrise/plugins/lsp.lua
@@ -29,6 +29,8 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
+                "eslint",
+                "tsserver",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
